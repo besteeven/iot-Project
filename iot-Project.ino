5901a80a2ad7b36698ee9592b3c04d2012655518
Michel BE <98329684+besteeven@users.noreply.github.com>
2026-01-15 12:57:14 +0100

    Add comments to iot-Project.ino for clarity
    Added comments to clarify project file purpose.

--- iot-Project.ino
+++ iot-Project.ino
@@ -1,1 +1,2 @@
-
+//fichier dummy portant le nom du projet pour éviter les erreurs de compilation avec Arduino
+// car Arduino a tendance a créer un nouveau dossier si aucun fichier .ino du même nom que le dossier actuel est trouvé dans le dosier
